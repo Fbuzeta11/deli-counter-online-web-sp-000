5ef250778f5bd07cbd5c6577cc255e3e7a92e445
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-04 21:58:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,17 @@
 katz_deli = ["Moshi", "Fayge", "Rivki"]
 
 def line_simple(array) 
-  current_line = "The simple line is currently:"
-  
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
 end 
 
 def take_a_number(line, new_person)
